c6c1bf37f66a4aff75c1c910b02ed900f3c2a81f
unmesh63 <54301810+unmesh63@users.noreply.github.com>
2019-11-27 11:20:11 +0530

    Add files via upload

---
+++ user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+    id:number;
+    name:string;
+    email:string;
+    password:string;
+  
+    constructor(){
+      this.id;
+      this.name = "";
+      this.email;
+      this.password;
+    }
+  }
---
+++ user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@angular/core";
+import 'rxjs/add/operator/toPromise';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+   public db: AngularFirestore,
+   public afAuth: AngularFireAuth
+ ){
+ }
+
+
+  getCurrentUser(){
+    return new Promise<any>((resolve, reject) => {
+      var user = firebase.auth().onAuthStateChanged(function(user){
+        if (user) {
+          resolve(user);
+          //window.alert(user.email);
+        } else {
+          reject('No user logged in');
+        }
+      })
+    })
+  }
+
+  
+}
